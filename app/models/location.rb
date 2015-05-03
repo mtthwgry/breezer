aaf22b03ca24409aa351562a455b17dee014175c
Matthew Gray <graymatthew3@gmail.com>
2015-05-02 20:38:46 -0700

    add #to_geojson to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,20 @@ class Location < ActiveRecord::Base
   validates :user_id, presence: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+  def to_geojson
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [self.longitude, self.latitude]
+      },
+      properties: {
+        id: self.id,
+        user_id: self.user_id,
+        name: self.user.name,
+        'marker-color': '#' + ("%06x" % (rand * 0xffffff))
+      }
+    }
+  end
 end
