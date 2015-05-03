d6f790316b359ca3de7a97136a4977922ab5dcdb
Matthew Gray <graymatthew3@gmail.com>
2015-05-03 10:18:51 -0700

    add transaction model and migration, update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502230939) do
+ActiveRecord::Schema.define(version: 20150503164702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20150502230939) do
   end
 
   add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "type"
+    t.decimal  "amount",     precision: 14, scale: 2
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "transactions", ["type"], name: "index_transactions_on_type", using: :btree
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
