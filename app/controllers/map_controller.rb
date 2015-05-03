870bc9ad2f37268e0316376bfd325773244cf54a
Matthew Gray <graymatthew3@gmail.com>
2015-05-02 20:40:21 -0700

    add json routes for getting all locations and locations based on a user's id

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,4 +3,14 @@ class MapController < ApplicationController
     @users = User.order(name: :asc)
     render :text => "", :layout => true
   end
+
+  def all_locations_for_user
+    @locations = Location.where(user_id: params[:id])
+    render json: @locations.map(&:to_geojson)
+  end
+
+  def all_locations
+    @locations = Location.all
+    render json: @locations.map(&:to_geojson)
+  end
 end
