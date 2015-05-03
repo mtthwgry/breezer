0dedc057d735e88fa44d82eca683159b54cfc6df
Matthew Gray <graymatthew3@gmail.com>
2015-05-03 16:01:17 -0700

    add transactions, charges, and earnings to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :locations
+  has_many :transactions
+  has_many :charges
+  has_many :earnings
 
   validates :name, presence: true, uniqueness: true
 end
