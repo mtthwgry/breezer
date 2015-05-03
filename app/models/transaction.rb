cbf91db299948ce8eb7ef229ee26f6e09a293120
Matthew Gray <graymatthew3@gmail.com>
2015-05-03 10:39:41 -0700

    add validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,11 @@
 class Transaction < ActiveRecord::Base
+  def self.types
+    %w( Charge Earning ).freeze
+  end
+
+  validates :user_id, presence: true
+  validates :amount, presence: true
+  validates :type, presence: true, inclusion: { in: Transaction.types,
+    message: "%{value} is not a valid transaction type" }
 
 end
