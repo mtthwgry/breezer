943de6ac104b52fe41cce699b57e6140ab622b46
Matthew Gray <graymatthew3@gmail.com>
2015-05-02 20:39:44 -0700

    add markers to map based on user filter

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,5 +2,39 @@ $(document).on("ready", function() {
   L.mapbox.accessToken = 'pk.eyJ1IjoibXR0aHdncnkiLCJhIjoibW82OTJicyJ9.ykVo84aS5xtK3jPy_iX6Sg';
 
   var map = L.mapbox.map('map', 'mtthwgry.bcb9b827', { zoomControl: false })
-  .setView([37.771167, -122.402504], 13);
+  .setView([37.771167, -122.402504], 12);
+
+  var featureLayer = L.mapbox.featureLayer().loadURL('/all_locations');
+
+  featureLayer.on('ready', function() {
+    this.setFilter(function() {
+      return false;
+    });
+
+    this.addTo(map);
+  });
+
+  var usersOn = [];
+
+  $('.user-list').on("click", '.user-list-item a', function(event) {
+    event.preventDefault();
+
+    var $userListItem = $(this).closest('.user-list-item');
+    $userListItem.toggleClass('active');
+
+    var userId = $(this).data('user-id');
+
+    if($userListItem.hasClass('active')) {
+      usersOn.push(userId);
+    } else {
+      var index = usersOn.indexOf(userId);
+      usersOn.splice(index, 1);
+    }
+
+    featureLayer.setFilter(function(f) {
+      return usersOn.indexOf(f.properties.user_id) > -1;
+    });
+
+    map.fitBounds(featureLayer.getBounds());
+  });
 });
